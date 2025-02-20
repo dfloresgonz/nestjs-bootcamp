b99020b7eda3165bb6aff8df760aa64182a0ba1e
Diegol <diegorflores@bcp.com.pe>
2025-02-19 20:56:09 -0500

    clase 2

--- productos-backend/src/app.controller.ts
+++ productos-backend/src/app.controller.ts
@@ -2,16 +2,16 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Product } from './utils/types';
 
-@Controller()
+@Controller('products')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('saludar')
   getHello(): string {
-    return `${this.appService.saludar()} sdfsdfsfsfsfsfsdffd`;
+    return `${this.appService.saludar()}`;
   }
 
-  @Get('products')
+  @Get('')
   getProducts(): Product[] {
     return this.appService.getProducts();
   }
