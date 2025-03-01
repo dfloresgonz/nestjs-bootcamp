ca11cdcb6b90072fd6f9b267f82c1b6a0682f0fe
Diegol <diegorflores@bcp.com.pe>
2025-02-28 20:09:18 -0500

    configs

--- proyecto/apps/productos-ms/src/main.ts
+++ proyecto/apps/productos-ms/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
-  const PORT = +process.env.PORT!;
+  const PORT = +process.env.PRODUCTOS_MS_PORT!;
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
